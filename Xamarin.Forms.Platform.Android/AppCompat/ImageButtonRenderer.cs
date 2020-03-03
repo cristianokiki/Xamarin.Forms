3b4cba31298cab80d10a47a2cde0e9fff5b257bd
Shane Neuville <shneuvil@microsoft.com>
2020-03-03 15:41:36 -0700

    Android fix WxH used during draw

--- Xamarin.Forms.Platform.Android/AppCompat/ImageButtonRenderer.cs
+++ Xamarin.Forms.Platform.Android/AppCompat/ImageButtonRenderer.cs
@@ -192,12 +192,23 @@ namespace Xamarin.Forms.Platform.Android
 			ElementChanged?.Invoke(this, new VisualElementChangedEventArgs(e.OldElement, e.NewElement));
 		}
 
+		protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+		{
+			base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+		}
 
+		protected override void OnLayout(bool changed, int left, int top, int right, int bottom)
+		{
+			base.OnLayout(changed, left, top, right, bottom);
+		}
+
+		int drawCount = 0;
 		public override void Draw(Canvas canvas)
 		{
 			if (Element == null)
 				return;
 
+			drawCount++;
 			var backgroundDrawable = _backgroundTracker?.BackgroundDrawable;
 			RectF drawableBounds = null;
 
@@ -206,8 +217,8 @@ namespace Xamarin.Forms.Platform.Android
 				if ((int)Forms.SdkInt >= 18 && backgroundDrawable != null)
 				{
 					var outlineBounds = backgroundDrawable.GetPaddingBounds(canvas.Width, canvas.Height);
-					var width = (float)MeasuredWidth;
-					var height = (float)MeasuredHeight;
+					var width = (float)canvas.Width;
+					var height = (float)canvas.Height;
 
 					var widthRatio = 1f;
 					var heightRatio = 1f;
@@ -242,6 +253,8 @@ namespace Xamarin.Forms.Platform.Android
 			{
 				base.Draw(canvas);
 			}
+
+			System.Diagnostics.Debug.Write($"END {drawCount}");
 		}
 
 		void IVisualElementRenderer.SetLabelFor(int? id)
